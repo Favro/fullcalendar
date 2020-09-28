a4da2527ddfcc57af0f5c6d1d09929246593d00b
Erik Olofsson <erik.olofsson@favro.com>
2020-09-28 17:18:10 +0200

    Make compatible with rzymek version of fullcalendar and update to 3.10.2

--- package.js
+++ package.js
@@ -1,19 +1,21 @@
 Package.describe({
   name: 'fullcalendar:fullcalendar',
-  version: '2.7.2',
+  version: '3.10.2',
   summary: 'Full-sized drag & drop event calendar',
   git: 'https://github.com/rgnevashev/fullcalendar.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.use(['momentjs:moment@2.5.0','jquery@1.11.4','templating@1.1.5']);
+  api.versionsFrom('METEOR@1.10.2');
+  api.use(['momentjs:moment','jquery@3.0.0','templating']);
   api.addAssets(["dist/fullcalendar.print.css"], 'client');
   api.addFiles([
+    "template.html",
+    "template.js",
     "dist/fullcalendar.css",
     "dist/fullcalendar.js",
-    "dist/lang-all.js",
+    "dist/locale-all.js",
     "dist/gcal.js",
-    "head.html"
   ], 'client');
 });
